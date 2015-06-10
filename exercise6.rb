d5445e67d64c6e7377c6bae4b177c1ff8b44ccfa
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-10 16:22:45 -0400

    Exercise6. Bananas are usb

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,12 @@
 apple_list = ["macbook", "iphone_6", "apple_watch"]
 puts apple_list
 
+apple_list << "earbugs"
+puts apple_list
 
+apple_list.include?("usb")
+if usb = false
+	puts "You should buy a pair"
+else usb = true
+	puts "Never Mind"
+end
