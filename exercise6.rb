26e886438e7cdae8a532d0ed762c64be5cbc235b
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-11 14:41:47 -0400

    Desregard the previous commit.  I corrected my * error and I am starting from the begining

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,23 @@
 apple_list = ["macbook", "iphone_6", "apple_watch"]
-puts apple_list
 
-apple_list << "earbugs"
-puts apple_list
+apple_list.each do |x|
+	x = "*"
 
-apple_list.include?("usb")
-if usb = false
-	puts "You should buy a pair"
-else usb = true
-	puts "Never Mind"
+puts "#{x} apple_list"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
