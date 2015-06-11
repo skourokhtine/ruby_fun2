905ae6d5c21c4a817fce1bca6faae90c417c9075
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-11 16:33:56 -0400

    Exercise6 Display second item from the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,28 +1,25 @@
-apple_list = ["macbook", "iphone_6", "apple_watch"]
-
-apple_list.each do |x|
-	# x = "macbook"
-	# y = "*"
-	puts "* #{x}"
+def display(list)
+	puts "The list:"
+	list.each do |x|
+		puts "* #{x}"
+	end
+	puts
 end
 
+apple_list = ["macbook", "iphone_6", "apple_watch"]
+display(apple_list)
+apple_list << "earbugs"
+display(apple_list)
 
+puts apple_list.count
 
+if apple_list.include?("usb") == false
+	puts "You should buy one"
+else  apple_list.include?("usb") == true
+	puts "Great, it could be handy"
+	end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+puts apple_list[1]
 
 
 
