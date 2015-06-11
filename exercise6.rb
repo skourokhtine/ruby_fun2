fa9305552af15c780ea0c3510546255c1dbfa8d1
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-11 17:14:14 -0400

    Exercise6 Completed

--- exercise6.rb
+++ exercise6.rb
@@ -12,14 +12,33 @@ apple_list << "earbugs"
 display(apple_list)
 
 puts apple_list.count
+puts
 
 if apple_list.include?("usb") == false
 	puts "You should buy one"
+	puts
 else  apple_list.include?("usb") == true
 	puts "Great, it could be handy"
 	end
 
 puts apple_list[1]
+puts
+
+apple_list.sort!
+display(apple_list)
+puts
+
+display apple_list
+
+apple_list.delete_at(2)
+puts
+
+display apple_list
+
+
+#This exercise was fun.
+
+
 
 
 
